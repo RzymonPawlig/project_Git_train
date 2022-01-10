e0856e5c9e3a6e4f498ec95fae22621c76cfb522
RzymonPawlig <97478169+RzymonPawlig@users.noreply.github.com>
2022-01-10 21:55:04 +0100

    Add files via upload

---
+++ GIT_OU_zal-master/cmake-build-debug/CMakeFiles/GIT_OU_zal.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GIT_OU_zal.dir/main.cpp.obj"
+  "GIT_OU_zal.exe"
+  "GIT_OU_zal.exe.manifest"
+  "GIT_OU_zal.lib"
+  "GIT_OU_zal.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GIT_OU_zal.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ GIT_OU_zal-master/cmake-build-debug/CMakeFiles/GIT_OU_zal.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for GIT_OU_zal.
+# This may be replaced when dependencies are built.
